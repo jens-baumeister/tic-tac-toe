2039515760a8c9e6a9c7ce98a3d6481c7449b0a8
Jens Baumeister <jens.baumeister@gmx.net>
2026-04-14 14:00:22 +0200

    add style and init function

--- script.js
+++ script.js
@@ -10,3 +10,33 @@ let fields = [
     null
 ];
 
+function init() {
+    render();
+}
+
+function render() {
+    let html = '<table>';
+
+    for (let i = 0; i < 3; i++) {
+        html += '<tr>';
+
+        for (let j = 0; j < 3; j++) {
+            let index = i * 3 + j;
+            let value = fields[index];
+
+            if (value === 'circle') {
+                html += '<td>o</td>';
+            } else if (value === 'cross') {
+                html += '<td>x</td>';
+            } else {
+                html += '<td></td>';
+            }
+        }
+
+        html += '</tr>';
+    }
+
+    html += '</table>';
+
+    document.getElementById('content').innerHTML = html;
+}
